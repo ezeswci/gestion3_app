bfa040c7c899aff30997699496f8af80f06efb67
ezeswci <ezequiel@swci.com.ar>
2016-07-27 16:56:18 -0300

    20160727_03

---
+++ js/validar_usuario.js
@@ -0,0 +1,43 @@
+function validar_usuario(){
+	//alert('validar usurio');
+	var usuario = document.getElementById("usuario").value;
+	var clave = document.getElementById("clave").value;
+	var empresa = document.getElementById("empresa").value;
+	var error = '';
+	if(empresa.length < 1){error = error+"<p>El campo <b>empresa</b> es obligatorio</p>"};
+	if(clave.length < 1){error = error+"<p>El campo <b>clave</b> es obligatorio</p>"};
+	if(usuario.length < 1){error = error+"<p>El campo <b>Usuario</b> es obligatorio</p>"};
+	if(error.length > 0){mostrar_alerta('Campos Obligatorios',error,BootstrapDialog.TYPE_DANGER);return}
+	var json=JSON.stringify({"usuario":usuario,"clave":clave,"empresa":empresa});
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+ 			 {// code for IE7+, Firefox, Chrome, Opera, Safari
+  				xmlhttp=new XMLHttpRequest();
+  			 }
+			else
+  			 {// code for IE6, IE5
+  				xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+ 			 }
+				xmlhttp.onreadystatechange=function()
+ 			 {
+				//alert('xmlhttp.readyState = '+xmlhttp.readyState+' xmlhttp.status = '+xmlhttp.status);
+  				if (xmlhttp.readyState==4 && xmlhttp.status==200)
+   			 {
+    			//value=xmlhttp.responseText;
+				respuesta = JSON.parse(xmlhttp.responseText);
+				if(respuesta.estado!=0){location.href='menu.html'}
+							else
+							var error = "Intentelo Nuevamente, de persistir verifique su conexi&oacute;n a internet. Si ese no es el inconveniente comunicarse con Sistemas"
+							{mostrar_alerta("<h2>Error en la Carga</h2>",error,BootstrapDialog.TYPE_DANGER);
+					}
+				
+    		  }
+     		}
+			//alert('ruta xxx = '+window.swciRuta);
+			//alert(json);
+			//----------------------
+			xmlhttp.open("POST",window.swciRuta+"gestion3/app_php/validar_usuario.php",true);
+			xmlhttp.setRequestHeader("Content-type","application/json;charset=UTF-8");
+			xmlhttp.withCredentials = "true";
+			xmlhttp.send(json);
+}
---
+++ js/variables/variablesTest.js
@@ -0,0 +1,3 @@
+// JavaScript Document
+window.test=true;
+if(window.test==true){window.swciRuta="../";}else{window.swciRuta="http://www.swci.com.ar/";}
