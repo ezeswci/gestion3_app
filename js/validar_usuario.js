d96a53907464fb031c505166b3cfee88fa70611f
ezeswci <ezequiel@swci.com.ar>
2016-08-05 08:35:27 -0300

    20160805_0835

--- js/validar_usuario.js
+++ js/validar_usuario.js
@@ -1,5 +1,5 @@
 function validar_usuario(){
-	alert('validar usurio');
+	//alert('validar usurio');
 	var www = document.getElementById("web").value;
 	var usuario = document.getElementById("usuario").value;
 	var clave = document.getElementById("clave").value;
